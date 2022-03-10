793f64b79f525aec323eefc7ef88aec57c475b9f
Ageng Anugrah Wardoyo Putra <agenganugrah@gmail.com>
2022-03-10 20:50:45 +0700

    Sudah pasrah

--- tugas_1/settings.py
+++ tugas_1/settings.py
@@ -127,8 +127,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if PRODUCTION:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
